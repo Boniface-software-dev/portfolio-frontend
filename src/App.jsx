59af6799e5544d65097592d4b2f13d4089e45943
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-09-12 18:06:55 -0500

    Editing browser rooter

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      
         <Routes>
           <Route path="/" element={<Portfolio />} />
         </Routes>
-      </BrowserRouter>
+      
     </div>
   );
 }
 
 export default App;
 
-// import React from "react";
-
-// function App() {
-//   return (
-//     <div className="text-center p-10">
-//       <h1 className="text-4xl font-bold text-blue-600">Hello Portfolio 🚀</h1>
-//       <p className="text-red-500">If this text is red, Tailwind works!</p>
-
-//     </div>
-//   );
-// }
-
-// export default App;
